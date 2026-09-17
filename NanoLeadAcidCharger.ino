fc3a3168d6dd6bb0cd1e8f9369438f2cb9b6ced1
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-09-17 14:08:55 +1000

    Start ESP8266 command interface from main sketch

--- NanoLeadAcidCharger.ino
+++ NanoLeadAcidCharger.ino
@@ -1,11 +1,15 @@
 #include "ChargerController.h"
+#include "Command.h"
 
 ChargerController charger;
+Command commandLink(charger);
 
 void setup() {
   charger.begin();
+  commandLink.begin();
 }
 
 void loop() {
   charger.update();
+  commandLink.update();
 }
